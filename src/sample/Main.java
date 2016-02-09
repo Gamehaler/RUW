cd9d2a9928297f1c743e19eab75db4727a71a64f
domin <dominikotocan@gmail.com>
2016-02-09 16:51:15 +0100

    Added a choice for category and resolution.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,9 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -17,11 +20,16 @@ import java.nio.file.Paths;
 
 public class Main extends Application implements EventHandler<ActionEvent> {
 
-    private Button downloadRadnomImageBtn, setAsWallpaperBtn;
-    private Text downloadStatusTxt, noPreviewText;
+    private Button downloadRadnomImageBtn, setAsWallpaperBtn, confirmSettingsBtn;
+    private Text downloadStatusTxt, noPreviewText, xText, urlText;
+    private TextField resWidthText, resHeightText;
     private ImageView previewImg;
-    private String imageUrl = "https://source.unsplash.com/random/1920x1080";
-    private String destinationFile = Paths.get(".").toAbsolutePath().normalize().toString() + "\\image.png";
+    private ChoiceBox<String> selectCategoryCB;
+    private String basicUrl = "https://source.unsplash.com/";
+    private String category;
+    private String resolution;
+    private String imageUrl;
+    private String destinationFile = Paths.get(".").toAbsolutePath().normalize().toString() + "\\image.jpg";
 
     File imageFile = new File(destinationFile);
     Image image = new Image(imageFile.toURI().toString());
@@ -48,7 +56,6 @@ public class Main extends Application implements EventHandler<ActionEvent> {
     public void handle(ActionEvent event) {
         if (event.getSource() == downloadRadnomImageBtn) {
             try {
-                downloadStatusTxt.setText("Downloading, please wait...");
                 Controller.downloadImage(imageUrl, destinationFile);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -60,6 +67,18 @@ public class Main extends Application implements EventHandler<ActionEvent> {
             }
         } else if (event.getSource() == setAsWallpaperBtn) {
             Controller.setAsWallpaper(destinationFile);
+        } else if (event.getSource() == confirmSettingsBtn) {
+            resolution = resWidthText.getText() + "x" + resHeightText.getText();
+
+            if (selectCategoryCB.getValue().equals("Random")) {
+                category = selectCategoryCB.getValue().toLowerCase() + "/";
+            } else {
+                category = "category/" + selectCategoryCB.getValue().toLowerCase() + "/";
+            }
+
+            imageUrl = basicUrl + category + resolution;
+            urlText.setText(imageUrl);
+            downloadRadnomImageBtn.setDisable(false);
         }
     }
 
@@ -74,44 +93,89 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 
         // Setting up GridPane items
         downloadRadnomImageBtn = new Button("Download image");
-        downloadRadnomImageBtn.setOnAction(this);
+
+        resWidthText = new TextField("1920");
+        xText = new Text(" x ");
+        resHeightText = new TextField("1080");
+
         setAsWallpaperBtn = new Button("Set as wallpaper");
-        setAsWallpaperBtn.setOnAction(this);
+
         setAsWallpaperBtn.setPrefWidth(500);
         downloadStatusTxt = new Text("Status");
 
-        // Download button on column 1, row 1
-        GridPane.setConstraints(downloadRadnomImageBtn, 0, 0);
+        urlText = new Text("");
 
-        // Status text on column 2, row 1
-        GridPane.setConstraints(downloadStatusTxt, 1, 0);
+        confirmSettingsBtn = new Button("Generate URL");
 
-        // Set as wallpaper button on column 1, row 2
-        GridPane.setConstraints(setAsWallpaperBtn, 0, 1, 3, 1);
+        selectCategoryCB = new ChoiceBox<>();
+
+        setAsWallpaperBtn.setOnAction(this);
+        downloadRadnomImageBtn.setOnAction(this);
+        confirmSettingsBtn.setOnAction(this);
+
+        selectCategoryCB.getItems().add("Random");
+        selectCategoryCB.getItems().add("Food");
+        selectCategoryCB.getItems().add("Nature");
+        selectCategoryCB.getItems().add("People");
+        selectCategoryCB.getItems().add("Technology");
+        selectCategoryCB.getItems().add("Objects");
+        selectCategoryCB.setValue("Random");
+
+        downloadRadnomImageBtn.setDisable(true);
+        // resWidthText.setDisable(true);
+        // resHeightText.setDisable(true);
+
+        // Width text columns 1 , row 1
+        GridPane.setConstraints(resWidthText, 0, 0);
+
+        // x text column 2, row 1
+        GridPane.setConstraints(xText, 1, 0);
+
+        // Height text columns 3, row 1
+        GridPane.setConstraints(resHeightText, 2, 0);
+
+        // Category selector column 4, row 1
+        GridPane.setConstraints(selectCategoryCB, 3, 0);
+
+        // Confirm button on column 1, row 2
+        GridPane.setConstraints(confirmSettingsBtn, 0, 1);
+
+        // Generate url button
+        GridPane.setConstraints(urlText, 1, 1, 3, 1);
+
+        // Download button on column 1, row 2
+        GridPane.setConstraints(downloadRadnomImageBtn, 0, 2);
+
+        // Status text on column 2, row 2
+        GridPane.setConstraints(downloadStatusTxt, 1, 2);
+
+        // Set as wallpaper button on column 1, row 3
+        GridPane.setConstraints(setAsWallpaperBtn, 0, 3, 4, 1);
 
         // resizes the image to have width of 500 while preserving the ratio and using
         // higher quality filtering method; this ImageView is also cached to
         // improve performance
         previewImg = new ImageView();
-
         previewImg.setFitWidth(500);
         previewImg.setPreserveRatio(true);
         previewImg.setSmooth(true);
         previewImg.setCache(true);
 
-        // Preview image on columns 1 and 2, row 1
-        GridPane.setConstraints(previewImg, 0, 2, 3, 1);
+        // Preview image on columns 1 and 2, row 4
+        GridPane.setConstraints(previewImg, 0, 5, 4, 1);
 
         if (imageFile.exists()) {
 
             previewImg.setImage(image);
             noPreviewText = new Text("");
             GridPane.setConstraints(noPreviewText, 0, 2, 3, 1);
-            grid.getChildren().addAll(downloadRadnomImageBtn, downloadStatusTxt, setAsWallpaperBtn, previewImg, noPreviewText);
+            grid.getChildren().addAll(downloadRadnomImageBtn, downloadStatusTxt, setAsWallpaperBtn, previewImg, noPreviewText,
+                    resWidthText, xText, resHeightText, confirmSettingsBtn, selectCategoryCB, urlText);
         } else {
             noPreviewText = new Text("No preview available\nPlease download the image first");
             GridPane.setConstraints(noPreviewText, 0, 2, 3, 1);
-            grid.getChildren().addAll(downloadRadnomImageBtn, downloadStatusTxt, setAsWallpaperBtn, previewImg, noPreviewText);
+            grid.getChildren().addAll(downloadRadnomImageBtn, downloadStatusTxt, setAsWallpaperBtn, previewImg, noPreviewText,
+                    resWidthText, xText, resHeightText, confirmSettingsBtn, selectCategoryCB, urlText);
         }
 
         return grid;
